4bbbcb4d2c75b44e10218f18c33c0c32304fdb71
sourabhT <sourabh.turkar@gmail.com>
2017-11-19 17:27:52 +0530

    .
    .

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var Login = require('../models/Login');
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
   Login.checkLoginCredential(req.body, function(err, result){
     if(err) {
       res.json(err);
