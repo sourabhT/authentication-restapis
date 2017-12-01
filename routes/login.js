ad355007c557a8016b8de8f40ee566244b9acc66
sourabhT <sourabh.turkar@gmail.com>
2017-12-01 21:38:36 +0530

    .
    .

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,32 @@ var express = require('express');
 var router = express.Router();
 var Login = require('../models/Login');
 
+// Asynchronous
+const crypto = require('crypto');
+
+
 router.post('/', function(req, res, next) {
   Login.checkLoginCredential(req.body, function(err, result){
     if(err) {
       res.json(err);
     } else {
-      res.json(result);
+
+
+      var username = result[0].username;
+      console.log(username);
+
+
+      crypto.randomBytes(256, (err, username) => {
+        if (err) throw err;
+        console.log(`${username.length} bytes of random data: ${username.toString('hex')}`);
+      });
+
+      if(result){
+        res.json(result);
+      }else{
+        res.json(result);
+      }
+
     }
   });
   return "count";
